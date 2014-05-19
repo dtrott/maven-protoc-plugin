a598fc11194ac44db458bee5c26c93921f36f2be
Sergei Ivanov <sergei_ivanov@mail.ru>
2014-05-20 00:57:50 +0100

    #5: Update site documentation

--- src/main/java/com/google/protobuf/maven/ProtocCompileMojo.java
+++ src/main/java/com/google/protobuf/maven/ProtocCompileMojo.java
@@ -8,7 +8,7 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 import java.io.File;
 
 /**
- * This mojo executes the {@code protoc} compiler for generating main java sources
+ * This mojo executes the {@code protoc} compiler for generating main Java sources
  * from protocol buffer definitions. It also searches dependency artifacts for
  * {@code .proto} files and includes them in the {@code proto_path} so that they can be
  * referenced. Finally, it adds the {@code .proto} files to the project as resources so
